1bb0ccb02c133641f58b0e553163cc4bb58a2160
marquesjosemar <126707247+marquesjosemar@users.noreply.github.com>
2026-07-08 21:28:57 -0300

    atualização crud

--- sistema-bancario/crud.php
+++ sistema-bancario/crud.php
@@ -151,7 +151,7 @@ se não, retorna mensagem de erro
 function depositar($usuarios, $nome, $valor) {
 
     if (empty($nome) || $valor <=0) {
-        return $usuarios;
+        return "usuario ou valor invalido";
     }
 
     foreach ($usuarios as $indice => $usuario) {
@@ -166,10 +166,247 @@ function depositar($usuarios, $nome, $valor) {
             }
         } 
     }
-    
+    return "usuario não encontrado";
 }
     
+/* Recebe: $usuarios, $nome e $valor.
+O nome não pode estar vazio.
+O valor deve ser maior que 0.
+Procurar o usuário pelo nome.
+Se não encontrar, retornar "Usuário não encontrado".
+Se encontrar, verificar se está ativo.
+Se estiver inativo, retornar "Usuário inativo".
+Verificar se o saldo é suficiente.
+Se o saldo for menor que o valor do saque, retornar "Saldo insuficiente".
+Se tudo estiver correto, diminuir o saldo e retornar a lista atualizada. 
+
+1. Qual é o objetivo?
+atualizar o saldo de usuario
+
+2. O que ela recebe?
+usuarios, nome, valor
+
+3. O que ela precisa encontrar?
+o usuario
+
+4. O que será alterado?
+o saldo do usuario
+
+5. Quais validações iniciais são necessárias?
+nome vazio? saldo disponivel?
+
+6. Depois de encontrar o usuário, quais regras devem ser verificadas?
+verificar se está ativo
+verificar se saldo é suficiente
+
+7. O que a função pode retornar?
+a lista de usuarios atualizada ou mensagem de erro
 
 
+-------ALGORITIMO -------------
+Recebe usuários, nome e valor.
+Se nome for vazio ou valor <= 0
+    retorna erro.
+Percorre a lista.
+Se encontrar o usuário
+    Se estiver inativo
+        retorna "Usuário inativo".
+    Se o saldo for insuficiente
+        retorna "Saldo insuficiente".
+    Atualiza o saldo.
+    Retorna a lista atualizada.
+Se terminar o foreach
+    Retorna "Usuário não encontrado".
+*/
 
 
+function sacar($usuarios, $nome, $valor) {
+
+    if (empty($nome) || $valor <=0) {
+        return "nome inexistente ou valor menor que zero";
+    }
+
+    foreach ($usuarios as $indice => $usuario) {
+        if ($usuario["nome"] === $nome) {
+            if (!$usuario["ativo"]) {
+                return "usuario inativo";
+            }
+            if ($usuario["saldo"] < $valor) {
+                return "saldo insuficiente";
+            }
+
+            $usuarios[$indice]["saldo"] -= $valor;
+            return $usuarios;
+        }
+    }
+
+    return "usuario não encontrado";
+
+}
+
+//FUNÇÃO TRANSFERIR
+
+/* Função: transferir()
+
+Regras
+
+A função recebe:
+
+$usuarios
+$remetente
+$destinatario
+$valor
+
+Ela deve:
+
+Validar os dados recebidos.
+Encontrar o remetente.
+Encontrar o destinatário.
+Verificar se ambos estão ativos.
+Verificar se o remetente possui saldo suficiente.
+Retirar o valor do remetente.
+Adicionar o valor ao destinatário.
+Retornar a lista atualizada.
+
+1. Qual é o objetivo?
+- atualizar saldo de remetente e destinatario
+
+2. O que ela recebe?
+- lista de usuarios, remetente, destinatario, valor
+
+3. O que ela precisa encontrar?
+- remetente, destinatario
+
+4. O que será alterado?
+- valor entre remetente e destinatario
+
+5. Quais validações iniciais são necessárias?
+remetente vazio? destinatario vazio? 
+(Antes do foreach.)
+
+6. Depois de encontrar os usuários, quais regras devem ser verificadas?
+- se ambos estão ativos
+- remetente possui saldo suficiente
+
+7. O que a função pode retornar?
+- lista atualizada
+
+ALGORITMO:
+Recebe usuários, remetente, destinatário e valor.
+Valida os parâmetros.
+Percorre a lista procurando o remetente.
+Se encontrar
+    guarda o índice.
+Percorre a lista procurando o destinatário.
+Se encontrar
+    guarda o índice.
+Se o remetente não foi encontrado
+    retorna erro.
+Se o destinatário não foi encontrado
+    retorna erro.
+Se o remetente estiver inativo
+    retorna erro.
+Se o destinatário estiver inativo
+    retorna erro.
+Se o saldo do remetente for insuficiente
+    retorna erro.
+Subtrai o valor do remetente.
+Adiciona o valor ao destinatário.
+Retorna a lista atualizada.*/
+
+function transferir($usuarios, $remetente, $destinatario, $valor) {
+
+    if (empty($remetente) || empty($destinatario) || $valor <= 0) {
+        return "dados invalidos";
+    }
+
+    $indiceRemetente = null;
+    $indiceDestinatario = null;
+    foreach ($usuarios as $indice => $usuario) {
+        if ($usuario["nome"] ===$remetente) {
+                $indiceRemetente = $indice;
+        } 
+    }
+
+    
+
+     foreach ($usuarios as $indice => $usuario) {
+        if ($usuario["nome"] ===$destinatario) {
+                $indiceDestinatario = $indice;
+        }
+        
+     } 
+     
+    if ($indiceRemetente === null || $indiceDestinatario === null) {
+        return "Remetente ou Destinatario não encontrado";
+    }
+    if (!$usuarios[$indiceRemetente]["ativo"] || !$usuarios[$indiceDestinatario]["ativo"]) {
+        return "Usuario não ativo";
+    }
+
+    if ($usuarios[$indiceRemetente]["saldo"] < $valor) {
+        return "saldo insuficiente";
+    } else {
+        $usuarios[$indiceRemetente]["saldo"] -= $valor;
+    }
+
+    
+    $usuarios[$indiceDestinatario]["saldo"] += $valor;
+    
+
+    return $usuarios;
+}
+
+
+/* 
+Função: buscarUsuario()
+
+Regras
+
+A função recebe:
+
+$usuarios
+$nome
+
+Ela deve:
+
+Validar o nome.
+Procurar o usuário.
+Se encontrar, retornar o usuário.
+Se não encontrar, retornar uma mensagem de erro.
+
+Observe que agora não vamos retornar a lista inteira. Vamos retornar apenas um usuário.
+
+Etapa 1 – Análise
+
+Responda apenas estas perguntas.
+
+1. Qual é o objetivo?
+- retornar um usuario
+2. O que ela recebe?
+-usuarios, nome
+3. O que ela precisa encontrar?
+- usuario
+4. O que será alterado?
+nada
+5. Quais validações iniciais são necessárias?
+-lista vazia? nome vazio?
+6. O que a função pode retornar? 
+- o usuario procurado
+*/
+
+function buscarUsuario($usuarios, $nome) {
+
+    if (empty($nome)) {
+        return "digite um nome";
+    }
+
+    foreach ($usuarios as $usuario) {
+        
+        if ($usuario["nome"] === $nome) {
+            return $usuario;
+        }
+    }
+
+    return "usuario não encontrado";
+}
